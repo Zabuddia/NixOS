6662fdd61c3f5a217071231fcaae1bd18f03a220
Alan <fife.alan@protonmail.com>
2025-08-03 20:47:59 -0500

    Added to distrobox.nix

--- distrobox.nix
+++ distrobox.nix
@@ -7,20 +7,39 @@
       debian = {
         image = "docker.io/library/debian:bookworm";
         extraPackages = [
-          "sudo"
-          "neofetch"
-          "git"
+          "sudo" "neofetch" "git" "curl" "htop"
+          "wget" "lsb-release" "ca-certificates"
+          "vim" "gnupg" "build-essential"
+          "fonts-dejavu-core" "xdg-utils" "dbus-x11"
+          "tree" "man-db" "iproute2" "strace"
+          "python3" "python3-pip"
         ];
         init = true;
       };
 
       ubuntu = {
-        image = "docker.io/library/ubuntu:22.04";
+        image = "docker.io/library/ubuntu:24.04";
         extraPackages = [
-          "curl"
-          "htop"
+          "sudo" "neofetch" "git" "curl" "htop"
+          "wget" "lsb-release" "ca-certificates"
+          "vim" "gnupg" "build-essential"
+          "fonts-dejavu-core" "xdg-utils" "dbus-x11"
+          "software-properties-common"
+          "tree" "man-db" "iproute2" "strace"
+          "python3" "python3-pip"
         ];
+        init = true;
       };
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/app-folders/folders/Remote" = {
+      name = "Remote";
+      apps = [
+        "debian.desktop"
+        "ubuntu.desktop"
+      ];
     };
   };
 }
